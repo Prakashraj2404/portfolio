516bdcca8da2b205e9fa0c9f09297df9d62699fe
Prakashraj2404 <prakashmadhaiyan02@gmail.com>
2024-04-03 20:49:52 +0530

    portfolio updates

--- src/portfolio.jsx
+++ src/portfolio.jsx
@@ -1,14 +1,14 @@
 import './css/portfolio.css';
 
+import portfolio from './accests/portfolio project.png';
 import project1 from './accests/restaurent project.png';
 import project2 from './accests/bootstrap project.png';
-import project3 from './accests/project-3.jpg';
-import project4 from './accests/project-4.jpg';
+import project3 from './accests/project-2.jpg';
+import project4 from './accests/petstore project.png';
 import project5 from './accests/project-5.jpg';
 import project6 from './accests/project-6.jpg';
 import project7 from './accests/project-7.jpg';
 import project8 from './accests/project-8.jpg';
-import project9 from './accests/project-9.jpg';
 
 
 export default function Portfolio(){
@@ -20,66 +20,78 @@ export default function Portfolio(){
             </div>
             <section className="projects-cont">
                 <article>
+                    <img src={portfolio}/>
+                    <div className='project-hover'>
+                        <h1>Portfolio</h1>
+                        <p>Personal portfolio - using React-Js FrameWorks</p>
+                        <a href='https://prakash-raj-portfolio-website.netlify.app/' target='_blank'><button>visit site</button></a>
+                    </div>
+                </article>
+
+                <article>
                     <img src={project1}/>
                     <div className='project-hover'>
-                    <p>preview</p>
+                    <h1>PROJECT 1</h1>
+                    <p>Restaurent website using HTML, CSS</p>
+                    <a href='https://prakashraj2404.github.io/Restaurent/' target='_blank'><button>visit site</button></a>
                     </div>
                 </article>
 
                 <article>
                     <img src={project2}/>
                     <div className='project-hover'>
-                    <p>preview</p>
+                    <h1>PROJECT 2</h1>
+                    <p>Learning Platform using HTML, Bootstrap</p>
+                    <a href='https://prakashraj2404.github.io/Bootstrap-Project/' target='_blank'><button>visit site</button></a>
                     </div>
                 </article>
 
                 <article>
                     <img src={project3}/>
                     <div className='project-hover'>
-                    <p>preview</p>
+                    <h1>PROJECT 3</h1>
+                    <p>Photoshop project(Group-project) using HTML, CSS,Bootstrap</p>
+                    <a href='https://prakashraj2404.github.io/Group-Project/' target='_blank'><button>visit site</button></a>
                     </div>
                 </article>
 
                 <article>
                     <img src={project4}/>
                     <div className='project-hover'>
-                    <p>preview</p>
+                    <h1>PROJECT 4</h1>
+                    <p>Petstore project(Group-project) using HTML, CSS,Bootstrap</p>
+                    <a href='https://prakashraj2404.github.io/Petstore-Project/' target='_blank'><button>visit site</button></a>
                     </div>
                 </article>
 
                 <article>
                     <img src={project5}/>
                     <div className='project-hover'>
-                    <p>preview</p>
+                    <h1>PROJECT 5</h1>
+                    <p>Js-Project On-Going</p>
+                    <a href='' target='_blank'><button>visit site</button></a>
                     </div>
                 </article>
+
 
                 <article>
                     <img src={project6}/>
                     <div className='project-hover'>
-                    <p>preview</p>
+                    <p>Coming Soon</p>
                     </div>
                 </article>
-
 
                 <article>
                     <img src={project7}/>
                     <div className='project-hover'>
-                    <p>preview</p>
+                    <p>Coming Soon</p>
                     </div>
                 </article>
 
                 <article>
                     <img src={project8}/>
                     <div className='project-hover'>
-                    <p>preview</p>
-                    </div>
-                </article>
-
-                <article>
-                    <img src={project9}/>
-                    <div className='project-hover'>
-                    <p>preview</p>
+                    <p>Coming Soon</p>
                     </div>
                 </article>
 
