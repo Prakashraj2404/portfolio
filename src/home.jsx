a4d6192c8767e19cdb1a1ec39d6a5e8500b9ec7a
Prakashraj2404 <prakashmadhaiyan02@gmail.com>
2024-04-03 18:56:54 +0530

    home page changes

--- src/home.jsx
+++ src/home.jsx
@@ -8,13 +8,22 @@ export default class Home extends React.Component{
     render(){
         return(
             <div className="home-container">
+
                 <div className="home-img">
                     <div className="home-clip"></div>
-                    <img src={Prakash} className="me"/>
+                    <div className="circle">
+                        <div className="rotate"></div>
+                        <img src={Prakash} className="me"/>
+                        <div className="content">
+                            <h5>PRAKASH</h5>
+                            <p>Web Developer</p>
+                            <button>CONTACT</button>
+                        </div>
+                    </div>
                 </div>
 
                 <div className="home-cont">
-                    <h1 className="red-bold-txt bold">I'm Prakash raj.<br/><span className="bold grey-txt-bold">web developer</span></h1>                   
+                    <h1 className="red-bold-txt bold">I'm Prakash raj.<br/><span className="bold grey-txt-bold">Web </span></h1>                   
                     <p className="home-p">I'm a Prakash Raj..., web designer & front-end developer focused on crafting clean & user-friendly experiences, I am passionate about building excellent software that improves the lives of those around me.</p>
                     <Link to="/about" className="aaa"><button className="home-button">more about me <a><TiArrowRightThick /></a></button></Link>
                 </div>
